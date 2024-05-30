35fbdc1dd8699e471ea0d0f63e0d8d22b6033f7b
Leandra Chevrier <leachevrier@hotmail.com>
2024-05-30 19:31:45 +0200

    View added with Tailwindcss

--- server/app.js
+++ server/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '..', 'client', 'views'));
+
+// Application's routes
+app.get('/',(req, res) =>{
+    res.render('layouts/accueil');
+})
+
 module.exports = app;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./client/**/*.{ejs,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
