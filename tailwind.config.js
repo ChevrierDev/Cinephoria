227d93b4c4b66a43a91586b0aa6930fedfc5065b
Leandra Chevrier <leachevrier@hotmail.com>
2024-05-30 19:56:00 +0200

    Tailwind personalized colors and font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./client/**/*.{ejs,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueOne: "#102C57",
+        goldOne: "#E3B04B",
+        redOne: '#C10E0E',
+        whiteOne: '#FEFAF6',
+        creamOne: '#EADBC8',
+        beigeOne: '#DAC0A3'
+      },
+      fontFamily: {
+        "arvo": ["Arvo", "serif"]
+      }
+    },
   },
   plugins: [],
 }
