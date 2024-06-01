2316951acec44eef1ac8bec83799b7c454c61fa5
Saintkops <erwanchevrier6@gmail.com>
2024-06-01 15:23:55 +0200

    landing page header carousel added

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,15 +1,47 @@
 const initApp = () => {
-    const hamburgerBtn = document.getElementById('hamburger-button');
-    const mobileMenu = document.getElementById('mobile-menu');
+  const hamburgerBtn = document.getElementById('hamburger-button');
+  const mobileMenu = document.getElementById('mobile-menu');
 
-    const toggleMenu = () => {
-        mobileMenu.classList.toggle('hidden');
-        mobileMenu.classList.toggle('flex');
-        hamburgerBtn.classList.toggle('toggle-btn')
-    }
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle('hidden');
+    mobileMenu.classList.toggle('flex');
+    hamburgerBtn.classList.toggle('toggle-btn');
+  }
 
-    hamburgerBtn.addEventListener('click', toggleMenu)
-    mobileMenu.addEventListener('click', toggleMenu)
+  hamburgerBtn.addEventListener('click', toggleMenu);
+  mobileMenu.addEventListener('click', toggleMenu);
+
+  // Initialiser le premier carrousel
+  const mainSlider = new Swiper('.main-slider', {
+    loop: true,
+    autoplay: {
+      delay: 2500,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  // Initialiser le second carrousel
+  const latestMovieSlider = new Swiper('.latest-movie-slider', {
+    loop: true,
+    slidesPerView: 3,
+    autoplay: {
+      delay: 2500,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    spaceBetween: -300, 
+  });
 }
 
-document.addEventListener('DOMContentLoaded', initApp)
+document.addEventListener('DOMContentLoaded', initApp);
