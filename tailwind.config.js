f0992ce549246d1cf3f4d7ad5f6623059fe16493
Saintkops <erwanchevrier6@gmail.com>
2024-06-02 22:30:48 +0200

    login page link animation added

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,10 +29,16 @@ module.exports = {
           '0%': { backgroundPosition: '200% 0' },
           '100%': { backgroundPosition: '0 0' },
         },
+
+        'text-fill-animation': {
+          '0%': { backgroundPosition: '200% 0' },
+          '100%': { backgroundPosition: '0 0' },
+        },
       },
       animation: {
        'open-menu':  'open-menu 0.5s ease-in-out forwards',
        'bg-fill-animation': 'bg-fill 1.5s forwards',
+       'text-fill-animation': 'text-fill-animation 1.5s ease forwards',
       },
       fontSize: {
         "xxs": "9px"
