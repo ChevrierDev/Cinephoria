b7d98f6d2d4637bcaf0794dbee822a4574d8a070
Saintkops <erwanchevrier6@gmail.com>
2024-06-06 16:46:19 +0200

    choose seat layout added

--- client/public/js/choose-seat.js
+++ client/public/js/choose-seat.js
@@ -2,22 +2,43 @@ const chooseSeatInitApp = () => {
     const playTrailerBtn = document.getElementById('start-trailer');
     const closeTrailerBtn = document.getElementById('close-video');
     const movieContainer = document.getElementById('movie');
+    const reserveMovieBtn = document.querySelectorAll('.reservation-confirmation');
+    const movieReservationMenu = document.getElementById('reservation-details');
     const body = document.body;
 
-    const toggleVideoTrailer = () => {
+    const toggleVideoTrailer = (event) => {
+        event.stopPropagation();  
         movieContainer.classList.toggle('hidden');
         body.classList.toggle('blur-body');
         body.classList.toggle('darken-background');
     }
 
-    const closeVideoTrailer = () => {
+    const closeVideoTrailer = (event) => {
+        event.stopPropagation();  
         movieContainer.classList.add('hidden');
         body.classList.remove('blur-body');
         body.classList.remove('darken-background');
     }
 
+    const toggleMovieReservationMenue = (event) => {
+        event.stopPropagation();  
+        movieReservationMenu.classList.toggle('hidden');
+        body.classList.toggle('darken-background');
+    }
+
+    const closeMovieReservationMenu = (event) => {
+        if (!movieReservationMenu.contains(event.target) && !event.target.classList.contains('reservation-confirmation')) {
+            movieReservationMenu.classList.add('hidden');
+            body.classList.remove('darken-background');
+        }
+    }
+
     playTrailerBtn.addEventListener('click', toggleVideoTrailer);
     closeTrailerBtn.addEventListener('click', closeVideoTrailer);
+    reserveMovieBtn.forEach(btn => {
+        btn.addEventListener('click', toggleMovieReservationMenue);
+    });
+    body.addEventListener('click', closeMovieReservationMenu);
 }
 
 document.addEventListener('DOMContentLoaded', chooseSeatInitApp);
