9f236cc23727fdbadae4735b6e450e58e28c397b
Saintkops <erwanchevrier6@gmail.com>
2024-06-13 00:50:36 +0200

    connection to mongoDB added

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
-const http = require('http');
-const app = require('../server/app.js');
+require('dotenv').config({path: '../.env'})
+const http = require("http");
+const app = require("../server/app.js");
+const mongoose = require('mongoose')
 const PORT = 3000;
 
 const server = http.createServer(app);
 
+//connextion to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((err) => console.log("Connexion à MongoDB échouée !", err));
+
 server.listen(PORT, () => {
-    console.log(`Vous êtes connecté au port ${PORT}`)
+  console.log(`Vous êtes connecté au port ${PORT}`);
 });
