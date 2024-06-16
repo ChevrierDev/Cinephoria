b8d07dd60af6fb4b006979f7070b82f57afe53a7
Saintkops <erwanchevrier6@gmail.com>
2024-06-16 02:45:51 +0200

    reset password layout and feature connected

--- server/routes/dashboard/users/userDash.routes.js
+++ server/routes/dashboard/users/userDash.routes.js
@@ -1,10 +1,21 @@
-const express = require('express');
+const express = require("express");
 const userDashRoutes = express.Router();
+const {
+  checkAuthenticated,
+  checkRole,
+} = require("../../../middlewares/autorisation/autorisation");
 
-userDashRoutes.get('/user/reset-pass',(req, res) =>{
-    res.render('dashboard/users/userResetPass', {
-        title: 'Réinitialiser votre mot de passe..'
+userDashRoutes.get(
+  "/user/reset-pass",
+  checkAuthenticated,
+  checkRole("user"),
+  (req, res) => {
+    const userId = req.user.sub;
+    res.render("dashboard/users/userResetPass", {
+      title: "Réinitialiser votre mot de passe..",
+      id: userId
     });
-});
+  }
+);
 
-module.exports = userDashRoutes
+module.exports = userDashRoutes;
