9a1a0c6c49513445222d2a31a182d6f175102788
Saintkops <erwanchevrier6@gmail.com>
2024-06-29 12:50:05 +0200

    connect API to film page

--- server/routes/films/films.routes.js
+++ server/routes/films/films.routes.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const filmsRoutes = express.Router();
+const {
+    getLastWedMovies,
+  } = require("../../controllers/movies/movies.controllers");
+  const decodeData = require("../../services/decodeData.services");
 
-filmsRoutes.get('/',(req, res) =>{
-    res.render('layouts/films', {
-        title: 'Les derniers films disponible.'
-    });
+filmsRoutes.get('/', async (req, res) =>{
+    try {
+        const lastMovies = await getLastWedMovies(req, res);
+        const decMovies = decodeData(lastMovies);        
+        res.render('layouts/films', {
+            title: 'Les derniers films disponible.',
+            movies: decMovies,
+        });
+    } catch (err) { 
+        console.log("Error while fetching last Wednesday movies:", err);
+        res.status(500).render("error", { error: "Internal server error" });
+    }
 });
 filmsRoutes.get('/disponibiliter',(req, res) =>{
     res.render('film/movie-availability', {
