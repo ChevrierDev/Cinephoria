ba4b543c5b6869603d3abe5821ff44c008d632e3
Saintkops <erwanchevrier6@gmail.com>
2024-07-04 23:53:42 +0200

    register form added

--- server/routes/login/login.routes.js
+++ server/routes/login/login.routes.js
@@ -2,8 +2,11 @@ const express = require("express");
 const loginRoutes = express.Router();
 
 loginRoutes.get("/", (req, res) => {
+  const redirectUrl = req.query.redirect || ''
   res.render("auth/login", {
+    
     title: "Connectez-vous à votre compte.",
+    redirectUrl 
   });
 });
 
