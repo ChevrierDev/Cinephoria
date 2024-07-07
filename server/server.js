6a62ae3d98fb9d504b0b76a0f559bdafcf3430b0
Saintkops <erwanchevrier6@gmail.com>
2024-07-07 17:52:23 +0200

    noSQL data feature added

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
-require('dotenv').config({path: '../.env'})
+require('dotenv').config({path: '../.env'});
 const http = require("http");
 const app = require("../server/app.js");
-const mongoose = require('mongoose')
-const PORT = 3000;
+const mongoose = require('mongoose');
+const connectDB = require('./config/mongodb.config.js');
+const PORT = 3030;
+require('./task/dataTransferTask.js');
 
 const server = http.createServer(app);
 
 //connexion to MongoDB
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("Connexion à MongoDB réussie !"))
-  .catch((err) => console.log("Connexion à MongoDB échouée !", err));
+connectDB()
 
 server.listen(PORT, () => {
   console.log(`Vous êtes connecté au port ${PORT}`);
